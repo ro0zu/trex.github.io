a93de03a68f7d22c67a30a08fe8c046ee2702d84
joseromeromolina <joseromeromolina35@gmail.com>
2025-05-25 22:31:30 +0200

    first commit

---
+++ js/codigo.js
@@ -0,0 +1,27 @@
+const image = document.querySelector('.image')
+
+//Altura Máxima del scroll.
+let maxScroll = document.documentElement.scrollHeight - window.innerHeight;
+
+window.addEventListener('resize', () => {
+    //Altura Máxima del scroll.
+    maxScroll = document.documentElement.scrollHeight - window.innerHeight;
+});
+
+window.addEventListener('scroll' ,() => {
+    const MAX_FRAMES = 36;
+    // Posición actual del scroll.
+    const scrollPosition = window.scrollY;
+
+    // Calcular el porcentaje del scroll.
+    const scrollFraction = scrollPosition / maxScroll;
+
+    // Que frame le toca?
+    const frame = Math.floor(scrollFraction * MAX_FRAMES) || 1;
+
+    // console.log(frame);
+    // Creamos el id del nombre del archivo.
+    const id = frame.toString().padStart(3, '0');
+
+    image.src = `/frames/ezgif-frame-${id}.jpg`;
+});
